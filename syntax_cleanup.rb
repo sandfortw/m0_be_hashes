95e0e3562789c0da82734f28b128992483dd7f51
Weston Sandfort <sandfortw@gmail.com>
2022-11-03 15:12:52 -0600

    Initial commit

--- syntax_cleanup.rb
+++ syntax_cleanup.rb
@@ -1,23 +1,28 @@
 # Clean Up the code below to improve readability.
-foods = {"apples": 23, 
-  "grapes": 507, 
-  "eggs": 48}
+foods = {
+  apples: 23, 
+  grapes: 507, 
+  eggs: 48
+  }
 
-person = { "height": "6 ft", "weight": "160 lbs" }
+person = { 
+  height_ft: 6, 
+  weight_lbs: 160
+  }
 
-post = { image_src: "./images/beach.png",
-
-
-   caption: "At the beach with my besties", timestamp: "4:37 PM August 13, 2019",
-        number_likes: 0,
-comments: [] }
-
-
-post2 =
-{
-      image_src: "./images/holiday-party.png",
-      caption: "What a great holiday party omg",
-      timestamp: "11:37 PM December 31, 2019",
-      number_likes: 13,
-      comments: []
+post = { 
+  image_src: "./images/beach.png",
+  caption: "At the beach with my besties", 
+  timestamp: "4:37 PM August 13, 2019",
+  number_likes: 0,
+  comments: [] 
 }
+
+
+post2 ={
+  mage_src: "./images/holiday-party.png",
+  caption: "What a great holiday party omg",
+  timestamp: "11:37 PM December 31, 2019",
+  number_likes: 13,
+  comments: []
+  }
